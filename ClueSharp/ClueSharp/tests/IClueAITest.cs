85a63a566af56cdaf929ee5b0486dcef15027ebd
Jack Grahl <jack.grahl@yahoo.co.uk>
2014-05-02 09:27:16 +0000

    Make Disprove work for ClueStick.
    Add generic tests for IClueAI. Tests currently dont work because of referential equality of Card.

---
+++ ClueSharp/ClueSharp/tests/IClueAITest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace ClueSharp.tests
+{
+  [TestFixture]
+  public abstract class ClueAITest<T> where T : IClueAI, new()
+  {
+    private T m_ai;
+
+    [SetUp]
+    public void Init()
+    {
+      m_ai = new T();
+    }
+
+    [Test]
+    public void DisproveTest()
+    {
+      m_ai.Reset(4, 3, new[] {Suspect.MissScarlet}, new Weapon[0], new[] {Room.Kitchen, Room.Conservatory, Room.Lounge} );
+      var disproof = m_ai.Disprove(0, new MurderSet(Suspect.MrGreen, Weapon.LeadPipe, Room.Kitchen));
+      Assert.AreEqual(new Card(Room.Kitchen), disproof);
+    }
+
+    [Test]
+    public void DisproveNoneTest()
+    {
+      m_ai.Reset(4, 3, new[] {Suspect.MissScarlet}, new Weapon[0], new[] {Room.Kitchen, Room.Conservatory, Room.Lounge} );
+      var disproof = m_ai.Disprove(0, new MurderSet(Suspect.MrGreen, Weapon.LeadPipe, Room.BilliardsRoom));
+      Assert.AreEqual(null, disproof);
+    }
+  }
+
+}
--- ClueSharp/ClueStick/ClueStick.cs
+++ ClueSharp/ClueStick/ClueStick.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ClueSharp;
+using ClueSharp.tests;
+using NUnit.Framework;
 
 namespace ClueStick
 {
   public class ClueStick : IClueAI
   {
+    private List<Suspect> m_suspects;
+    private List<Weapon> m_weapons;
+    private List<Room> m_rooms;
+
     public ClueStick()
     {
     }
 
     public void Reset(int n, int i, IEnumerable<Suspect> suspects, IEnumerable<Weapon> weapons, IEnumerable<Room> rooms)
     {
+      m_suspects = suspects.ToList();
+      m_weapons = weapons.ToList();
+      m_rooms = rooms.ToList();
     }
 
     public void Suggestion(int suggester, MurderSet suggestion, int? disprover, Card disproof)
@@ -35,7 +45,22 @@ namespace ClueStick
 
     public Card Disprove(int player, MurderSet suggestion)
     {
-      throw new NotImplementedException();
+      if (m_suspects.IndexOf(suggestion.Suspect) != -1)
+      {
+        return new Card(suggestion.Suspect);
+      }
+      if (m_weapons.IndexOf(suggestion.Weapon) != -1)
+      {
+        return new Card(suggestion.Weapon);
+      }
+      if (m_rooms.IndexOf(suggestion.Room) != -1)
+      {
+        return new Card(suggestion.Room);
+      }
+      return null;
     }
   }
+
+  public class ClueStickTest : ClueAITest<ClueStick>
+  {}
 }
