362623f325aac1cdd7fdfcccb7f901d0d6fea0db
Jack Grahl <jack.grahl@yahoo.co.uk>
2014-05-02 20:19:04 +0000

    Add ClueBat, slightly more clueful than ClueStick.
    This is a very simple AI which has no strategy, except to try and exploit a flaw in ClueStick.

---
+++ ClueSharp/ClueBat/ClueBat.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClueSharp;
+using ClueSharp.tests;
+
+namespace ClueBat
+{
+  public class ClueBat : IClueAI
+  {
+    private List<Suspect> m_suspects;
+    private List<Weapon> m_weapons;
+    private List<Room> m_rooms;
+    private readonly IEnumerator<MurderSet> m_suggestions;
+    private MurderSet? m_accusation;
+    private bool m_firstMove = true;
+
+    public ClueBat()
+    {
+      m_suggestions = MurderSet.AllSuggestions.GetEnumerator();
+    }
+
+    public void Reset(int n, int i, IEnumerable<Suspect> suspects, IEnumerable<Weapon> weapons, IEnumerable<Room> rooms)
+    {
+      m_suspects = suspects.ToList();
+      m_weapons = weapons.ToList();
+      m_rooms = rooms.ToList();
+    }
+
+    public void Suggestion(int suggester, MurderSet suggestion, int? disprover, Card disproof)
+    {
+      if (disproof == null)
+      {
+        m_accusation = suggestion;
+      }
+    }
+
+    public void Accusation(int accuser, MurderSet suggestion, bool won)
+    {
+    }
+
+    public MurderSet Suggest()
+    {
+      // This takes advantage of the flaw in ClueStick to usually beat that AT
+      // 
+      if (m_firstMove)
+      {
+        if (m_suspects.Any() && m_weapons.Any() && m_rooms.Any())
+        {
+          return new MurderSet(m_suspects.First(), m_weapons.First(), m_rooms.First());
+        }
+        m_firstMove = false;
+      }
+
+      if (!m_suggestions.MoveNext())
+      {
+        throw new Exception("Run out of suggestions");
+      }
+      return m_suggestions.Current;
+    }
+
+    public MurderSet? Accuse()
+    {
+      return m_accusation;
+    }
+
+    public Card Disprove(int player, MurderSet suggestion)
+    {
+      if (m_suspects.IndexOf(suggestion.Suspect) != -1)
+      {
+        return new Card(suggestion.Suspect);
+      }
+      if (m_weapons.IndexOf(suggestion.Weapon) != -1)
+      {
+        return new Card(suggestion.Weapon);
+      }
+      if (m_rooms.IndexOf(suggestion.Room) != -1)
+      {
+        return new Card(suggestion.Room);
+      }
+      return null;
+    }
+
+    public class ClueStickTest : ClueAITest<ClueBat>
+    {
+    }
+  }
+}
