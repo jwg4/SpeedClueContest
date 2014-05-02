94d51f36d5efb1dd967355eeffbd3921d9243cf9
Jack Grahl <jack.grahl@yahoo.co.uk>
2014-05-02 09:57:19 +0000

    Move file to reflect change in name of class.

--- ClueSharp/ClueSharp/tests/ClueAITest.cs
+++ ClueSharp/ClueSharp/tests/ClueAITest.cs
@@ -14,36 +14,19 @@ namespace ClueSharp.tests
     }
 
     [Test]
-    public void TestAccuse()
+    public void DisproveTest()
     {
-      m_ai.Reset(4, 3, new[] { Suspect.MissScarlet }, new Weapon[0], new[] { Room.Kitchen, Room.Conservatory, Room.Lounge });
-      m_ai.Accuse();
-    }
-
-    [Test]
-    public void TestDisprove()
-    {
-      m_ai.Reset(4, 3, new[] { Suspect.MissScarlet }, new Weapon[0], new[] { Room.Kitchen, Room.Conservatory, Room.Lounge });
+      m_ai.Reset(4, 3, new[] {Suspect.MissScarlet}, new Weapon[0], new[] {Room.Kitchen, Room.Conservatory, Room.Lounge} );
       var disproof = m_ai.Disprove(0, new MurderSet(Suspect.MrGreen, Weapon.LeadPipe, Room.Kitchen));
       Assert.AreEqual(new Card(Room.Kitchen), disproof);
     }
 
     [Test]
-    public void TestDisproveReturnsNullWhenNoneAvailable()
+    public void DisproveNoneTest()
     {
-      m_ai.Reset(4, 3, new[] { Suspect.MissScarlet }, new Weapon[0], new[] { Room.Kitchen, Room.Conservatory, Room.Lounge });
+      m_ai.Reset(4, 3, new[] {Suspect.MissScarlet}, new Weapon[0], new[] {Room.Kitchen, Room.Conservatory, Room.Lounge} );
       var disproof = m_ai.Disprove(0, new MurderSet(Suspect.MrGreen, Weapon.LeadPipe, Room.BilliardsRoom));
       Assert.AreEqual(null, disproof);
-    }
-
-    [Test]
-    public void DontSuggestTheSameThingTwice()
-    {
-      m_ai.Reset(4, 3, new[] { Suspect.MissScarlet }, new Weapon[0], new[] { Room.Kitchen, Room.Conservatory, Room.Lounge });
-      var suggestion1 = m_ai.Suggest();
-      m_ai.Suggestion(3, suggestion1, 1, new Card(suggestion1.Suspect));
-      var suggestion2 = m_ai.Suggest();
-      Assert.AreNotEqual(suggestion1, suggestion2);
     }
   }
 
