0b6c15ac060667ef1f6f7b1b4fb5d95656a6dfc9
Jack Grahl <jack.grahl@yahoo.co.uk>
2014-05-02 19:23:20 +0000

    Change Program structure so that we can make different bots at once.
    ClueBot.Program becomes ClueBot.ProgramTemplate<>. For each implementation of IClueAI, we can derive from this class to get a Program class which will run the client with that AI.

---
+++ ClueSharp/ClueBot/ProgramTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using ClueSharp;
+
+namespace ClueBot
+{
+  public abstract class ProgramTemplate<T> where T : IClueAI, new() 
+  {
+    protected internal ClueClient m_clueClient;
+
+    public void DoMain(string[] args)
+    {
+      string identifier = args[0];
+      int port = Int32.Parse(args[1]);
+      var client = new TcpClient("localhost", port);
+      if (!client.Connected)
+      {
+        throw new Exception();
+      }
+      m_clueClient = new ClueClient(client.GetStream());
+      
+      m_clueClient.Handshake(identifier);
+
+      bool end = false;
+      var ai = new T();
+      while (!end)
+      {
+        end = m_clueClient.ProcessMessage(ai);
+      }
+
+    }
+  }
+}
