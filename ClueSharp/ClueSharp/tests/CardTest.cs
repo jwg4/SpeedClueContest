3bf54951f6ed1670e7d8a386ca641941821e0087
Jack Grahl <jack.grahl@yahoo.co.uk>
2014-05-02 09:29:51 +0000

    Add test for value-based equality of Card.

---
+++ ClueSharp/ClueSharp/tests/CardTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ClueSharp.tests
+{
+  [TestFixture]
+  public class CardTest
+  {
+    [Test]
+    public void EqualityTest()
+    {
+      Assert.AreEqual(new Card(Room.Kitchen), new Card(Room.Kitchen));
+    }
+  }
+}
