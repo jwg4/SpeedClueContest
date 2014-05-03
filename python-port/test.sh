ea6b413bf95523febbb55844b80f7964c3d9b0de
Jack Grahl <jack.grahl@yahoo.co.uk>
2014-05-03 01:29:50 +0000

    ClueStick vs ClueBat vs CluePaddle

--- python-port/test.sh
+++ python-port/test.sh
@@ -5,7 +5,7 @@ else
     PORT=8889
 fi
 BUF=--buf
-python3 -m speedclue $BUF --port $PORT --count 500 \
-    ../ClueSharp/ClueStick/bin/Debug/ClueStick.exe\
+python3 -m speedclue $BUF --port $PORT --count 5 \
+    ../ClueSharp/CluePaddle/bin/Debug/CluePaddle.exe\
     ../ClueSharp/ClueStick/bin/Debug/ClueStick.exe\
     ../ClueSharp/ClueBat/bin/Debug/ClueBat.exe
